03963bfceeafecdf39b5a145cb605b567a0054ba
yeongasleep <kangyeonghwan95@gmail.com>
2020-11-21 09:42:04 +0900

    비밀정보 보호

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+// 배포후 개발
+module.exports = {
+    mongoURI: process.env.MONGO_URI
+}
--- models/User.js
+++ models/User.js
@@ -2,36 +2,36 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     name: {
-        type: String,
-        maxlength: 50,
+        type:String,
+        maxlength:50
     },
     email: {
-        type: String,
-        trim: true,
-        unique: true
+        type:String,
+        trim:true,
+        unique: 1 
     },
     password: {
         type: String,
-        minlength:5
+        minglength: 5
     },
     lastname: {
-        type: String,
+        type:String,
         maxlength: 50
     },
-    role: {
-        type: Number,
-        default: 0
+    role : {
+        type:Number,
+        default: 0 
     },
     image: String,
-    token: {
-        type: String
+    token : {
+        type: String,
     },
-    tokenExp: {
+    tokenExp :{
         type: Number
     }
 })
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+module.exports = { User };
 
